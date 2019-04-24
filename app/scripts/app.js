4e625e733bcd04c794cdcb8ca1e1ea78df814430
amirgc <gcamir15@gmail.com>
2019-04-24 17:46:33 -0400

    first push with route config

---
+++ app/scripts/app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * @ngdoc overview
+ * @name angularJsStarterKitApp
+ * @description
+ * # angularJsStarterKitApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module("angularJsStarterKitApp", [
+    "ngAnimate",
+    "ngCookies",
+    "ngResource",
+    "ngRoute",
+    "ngSanitize",
+    "ngTouch"
+  ])
+  .config([
+    "$routeProvider","$locationProvider",
+    function($routeProvider,$locationProvider) {
+      $locationProvider.hashPrefix('!');
+      $routeProvider
+      .when("/", {
+        templateUrl: "views/main.html",
+        controller: "MainCtrl",
+        controllerAs: "main"
+      })
+      .when("/contact", {
+        templateUrl: "views/contact.html",
+        controller: "MainCtrl",
+        controllerAs: "main"
+      }).when("/about", {
+        templateUrl: "views/about.html",
+        controller: "MainCtrl",
+        controllerAs: "main"
+      })
+      .otherwise({
+        redirectTo: "/"
+      });
+    }
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsStarterKitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJsStarterKitApp
+ */
+angular.module('angularJsStarterKitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
